6bcc1e0a9f79f3e98477e688939f25f432207125
msunilhyd <msunilhyd@gmail.com>
2021-12-01 07:36:57 -0600

    Generic commit

--- src/samples/playlists.js
+++ src/samples/playlists.js
@@ -10,8 +10,8 @@ const playlists = {
         // https://www.youtube.com/watch?v=poJlUQt8KTk
         // https://www.youtube.com/watch?v=N86ZRsX6SwA
 
-
-        ['People Get Up And Drive Your Funky Soul', 'ENGLISH', '2012', 'Leonard Cohen', 'ttEMYvpoR-k', ''],
+        ['Chukkala Chunni', 'TELUGU', '2018', 'Chaitan Bharadwaj', '1tL0rsKvZFE', ''],
+        ['People Get Up And Drive Your Funky Soul', 'ENGLISH', '2012', 'Leonard Cohen', '0oxt-WtC6Oc', ''],
         ['Premante Suluvu', 'TELUGU', '2001', 'Mani Sharma', 'IBfoSYDwsyQ', 'Kushi', 0, 372],
         ['Hallelujah ', 'ENGLISH', '2012', 'Leonard Cohen', 'ttEMYvpoR-k', ''],
         ['Abhi Mujh Mein Kahin Cover', 'HINDI', '2012', 'Ajay-Atul', '_QDAgv99WTc', 'Agneepath'],
