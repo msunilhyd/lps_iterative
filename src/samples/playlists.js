5e4fb952c123e749143ac1c3cf857b6f757b9900
Sunil Mocharla <msunilhyd@gmail.com>
2022-10-19 20:38:43 -0500

    Update playlists.js

--- src/samples/playlists.js
+++ src/samples/playlists.js
@@ -1274,7 +1274,8 @@ const playlists = {
         ['Piano Man', 'ENGLISH', '-', 'Billy Joel', 'QwVjTlTdIDQ', '-'],
         ['Pehli Dafa', 'ENGLISH', '-', 'Billy Joel', '70FbfOQXPwk', '-'],
         ['Kesariya (Dance Mix)', 'ENGLISH', '-', 'Billy Joel', 'K3B8-klo5xc', '-'],
-      
+        ['JAB TAK', 'ENGLISH', '-', 'Billy Joel', 'K-Ts-NFR62o', '-'],
+
 
         ['Yuvan Jukebox', 'TELUGU', '2007', 'Devi Sri Prasad', 'xBLsWs-cwgc', 'Shankardada Zindabad'],
       
