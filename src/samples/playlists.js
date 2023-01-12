57e7f961d78155ba25f1d84e6c46ffe82092e318
Sunil Mocharla <msunilhyd@gmail.com>
2023-01-11 23:08:08 -0600

    Update playlists.js

--- src/samples/playlists.js
+++ src/samples/playlists.js
@@ -80,6 +80,7 @@ const playlists = {
         ['Kottaga BGM 1', 'TELUGU', '1988', 'IlayaRaaja', 's9h0kmdSXx8 1', 'Swarna Kamalam', 0, 28],
         ['Was He Slow', 'TAMIL', '2005', 'Harris Jayaraj', '3igDZrG3DTQ', 'Ghajini'],
         ['Kesariya', 'TAMIL', '2015', 'Harris Jayaraj', 'g6fnFALEseI', 'Anegan'],
+	['Nijamena', 'TAMIL', '2005', 'Harris Jayaraj', 'vZgML3bfchk', 'Ghajini'],
         ['Vayyarala Jabilli BGM 2', 'TELUGU', '2011', 'Mani Sharma', 'Yg-8_MfqDQc 2', 'Teenmaar', 89, 122],
         ['Akhiyaan Milavanga', 'TELUGU', '2001', 'Mani Sharma', 'O9zBPcU5g60', 'Kushi'],
         ['Ghajini BGM 9', 'TAMIL', '2005', 'Harris Jayaraj', 'faOR9KKzqLs 34', 'Ghajini', 1226, 1265],
