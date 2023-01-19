d6acaf6d5e86f993f19dfe4b7c3a4313cced98c7
Sunil Mocharla <msunilhyd@gmail.com>
2023-01-18 19:40:23 -0600

    Update playlists.js

--- src/samples/playlists.js
+++ src/samples/playlists.js
@@ -20,6 +20,7 @@ const playlists = {
         ['Mellisaye (Reprise) Manonmani', 'TAMIL', '2018', 'Yeshwanth | Nikhil Mathew', 'Jv477p7GpfM', '-', 6, 170],
         ['Toh Phir Aao Remix', 'HINDI', '2007', 'Pritam', 'Ahn5Uv9lppA', 'Awarapan'],
         ['Chitti Aduga', 'HINDI', '2007', 'Pritam', 'qD8sPWTekDQ', 'Awarapan'],
+        ['Tere Bin', 'HINDI', '2007', 'Pritam', 'aeNq_hB9TXI', 'Awarapan'],
         ['Akasam Ammaiyaite', 'HINDI', '2007', 'Pritam', 'p6Ejrw6VZKk', 'Awarapan'],
         ['Gajini lovely bgm', 'TAMIL', '2005', 'Harris Jayaraj', '0Pnqbi7XwUw', 'Ghajini'],
         ['Yung Gravy - Betty', 'TAMIL', '2005', 'Harris Jayaraj', 'O6h0hmsTAyY', 'Ghajini'],
