01224254167b07a61e6dec5d63864eb146f91e2a
Sunil Mocharla <msunilhyd@gmail.com>
2023-05-30 17:04:53 -0500

    Update playlists.js

--- src/samples/playlists.js
+++ src/samples/playlists.js
@@ -15,6 +15,7 @@ const playlists = {
         ['Shaamat', 'TELUGU', '1996', 'A R Rahman', 'd_Xbd2I5Zxk', 'Prema Desam'],
         ['The Day Of (Oceans Twelve)', 'TELUGU', '1996', 'A R Rahman', 'GnOKz3FYAPo', 'Prema Desam'],
         ['Galliyan Returns', 'TELUGU', '1996', 'A R Rahman', 'oPS-ayK43zI', 'Prema Desam'],
+        ['Another way to die', 'TELUGU', '1996', 'A R Rahman', '4_B4PtwE-QA', 'Prema Desam'],
         ['O Rendu Prema Meghaalila Reprise', 'TELUGU', '1996', 'A R Rahman', 'zZw3ECvdCM8', 'Prema Desam'],
         ['La Caution - Thé à la Menthe', 'TELUGU', '1996', 'A R Rahman', 'k4Xx0k_TVY0', 'Prema Desam'],
         ['Mustafa Mustafa', 'TELUGU', '1996', 'A R Rahman', 'iB8g5cB8am8', 'Prema Desam', 5, 366],
